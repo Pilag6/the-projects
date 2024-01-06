95e345c6ccf7047bce12f3d85470a8620420b051
Pilag6 <pilag6@gmail.com>
2024-01-06 19:53:43 +0100

    Add dark mode functionality

---
+++ js/darkMode.js
@@ -0,0 +1,43 @@
+// DARK MODE
+
+const lightMode = document.querySelector('i.fa-sun');
+const darkMode = document.querySelector('i.fa-moon');
+const aside = document.querySelector('aside');
+const body = document.querySelector('body');
+const mainTitleH1 = document.querySelector('.main-title h1');
+const mainTitle = document.querySelector('.main-title');
+const cardsContainer = document.querySelectorAll('.cards-container article');
+const goToProject = document.querySelectorAll('.cards-container article a');
+
+export function toggleDarkMode() {
+    lightMode.style.display = 'flex';
+    darkMode.style.display = 'none';
+    aside.style.backgroundColor = '#2a2828';
+    body.style.backgroundColor = '#131212';
+    mainTitleH1.style.color = '#f5f5f5';
+    mainTitle.style.backgroundColor = '#2a2828';
+    goToProject.forEach(link => {
+        link.style.color = '#f5f5f5';
+    });
+
+    cardsContainer.forEach(card => {
+        card.style.border = '1px solid #f5f5f52d';
+        card.style.backgroundColor = '#2a2828';
+        card.style.color = '#f5f5f5';
+    });
+}
+
+export function toggleLightMode() {
+    lightMode.style.display = 'none';
+    darkMode.style.display = 'flex';
+    body.style.backgroundColor = '#f5f5f5';
+    aside.style.backgroundColor = '#131212';
+    mainTitleH1.style.color = '#131212';
+    mainTitle.style.backgroundColor = '#f5f5f5';
+
+    cardsContainer.forEach(card => {
+        card.style.border = 'none';
+        card.style.backgroundColor = '#f5f5f5';
+        card.style.color = '#131212';
+    });
+}
--- main.js
+++ main.js
@@ -1,29 +1,10 @@
+import { toggleDarkMode } from "./js/darkMode.js";
+import { toggleLightMode } from "./js/darkMode.js";
+
 // DARK MODE
 
-const lightMode = document.querySelector('i.fa-sun');
-const darkMode = document.querySelector('i.fa-moon');
-const aside = document.querySelector('aside');
-const main = document.querySelector('main');
-const body = document.querySelector('body');
-const mainTitleH1 = document.querySelector('.main-title h1');
-const mainTitle = document.querySelector('.main-title');
+const lightMode = document.querySelector("i.fa-sun");
+const darkMode = document.querySelector("i.fa-moon");
 
-lightMode.addEventListener('click', () => {
-    lightMode.style.display = 'none';
-    darkMode.style.display = 'flex';
-    body.style.backgroundColor = '#f5f5f5';
-    aside.style.backgroundColor = '#131212';
-    mainTitleH1.style.color = '#131212';
-    mainTitle.style.backgroundColor = '#f5f5f5';
-   
-});
-
-darkMode.addEventListener('click', () => {
-    lightMode.style.display = 'flex';
-    darkMode.style.display = 'none';
-    aside.style.backgroundColor = '#2a2828';
-    body.style.backgroundColor = '#131212';
-    mainTitleH1.style.color = '#f5f5f5';
-    mainTitle.style.backgroundColor = '#2a2828';
-});
-
+lightMode.addEventListener("click", toggleLightMode);
+darkMode.addEventListener("click", toggleDarkMode);
