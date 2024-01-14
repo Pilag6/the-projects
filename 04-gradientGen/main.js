0fcd9533dbc37f67b8a52ae65dfdbe0374be1d44
Pilag6 <pilag6@gmail.com>
2024-01-14 21:23:39 +0100

    Update gradientGen UI layout

---
+++ 04-gradientGen/main.js
@@ -0,0 +1,27 @@
+// References
+
+/* 
+1- Display Sample
+2- Directions Select Box
+3- Colors Inputs
+4- Code display Box
+5- Random Btn
+6- Copy Btn
+*/
+
+// Functions
+
+/* 
+1- Random Color Generator
+2- Generate Gradient
+3- Copy to Clipboard
+*/ 
+
+// Event Listeners
+
+/* 
+1- Direction Select Box
+2- Color Inputs
+3- Random Btn
+4- Copy Btn
+*/
