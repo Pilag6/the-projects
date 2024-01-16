36d9fae64575b5659561194bfcf7b79a5928fcb7
Pilag6 <pilag6@gmail.com>
2024-01-16 18:19:02 +0100

    Create Gradient

--- 04-gradientGen/main.js
+++ 04-gradientGen/main.js
@@ -1,27 +1,60 @@
-// References
+const colorDisplay = document.querySelector(".color-display");
+const directionSelect = document.querySelector("select");
+const colorInputs = document.querySelectorAll(".box-selector input");
+const codeBox = document.querySelector(".code-box");
+const randomBtn = document.querySelector("#random-btn");
+const copyBtn = document.querySelector("#copy-btn");
+const mainContainer = document.querySelector(".container");
 
-/* 
-1- Display Sample
-2- Directions Select Box
-3- Colors Inputs
-4- Code display Box
-5- Random Btn
-6- Copy Btn
-*/
+const generateGradient = (isRandom) => {
+    if (isRandom) {
+        colorInputs[0].value = getRandomColor(); // #fa3424
+        colorInputs[1].value = getRandomColor(); // #fa3424
+    }
 
-// Functions
+    const gradient = `linear-gradient(${directionSelect.value}, ${colorInputs[0].value}, ${colorInputs[1].value})`;
 
-/* 
-1- Random Color Generator
-2- Generate Gradient
-3- Copy to Clipboard
-*/ 
+    colorDisplay.style.background = gradient;
+    mainContainer.style.background = gradient;
 
-// Event Listeners
+    codeBox.textContent = `background: ${gradient}`;
+};
 
-/* 
-1- Direction Select Box
-2- Color Inputs
-3- Random Btn
-4- Copy Btn
-*/
+const getRandomColor = () => {
+    let options = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "a", "b", "c", "d", "e", "f"];
+
+    let hex = "#";
+
+    for (let i = 0; i < 6; i++) {
+        hex += options[Math.floor(Math.random() * options.length)];
+        // console.log(`${i}: ${hex}`);
+    }
+
+    return hex;
+};
+
+function copyGradient() {
+    navigator.clipboard.writeText(codeBox.textContent);
+
+    copyBtn.textContent = "Gradient Copied!";
+    copyBtn.style.background = "#01857c";
+    codeBox.style.fontWeight = "bold";
+
+    setTimeout(() => {
+        copyBtn.textContent = "Copy Gradient";
+        copyBtn.style.background = "#01b7ab";
+        codeBox.style.fontWeight = "normal";
+    }, 2000);
+};
+
+
+colorInputs.forEach((color) => {
+    color.addEventListener("input", () => generateGradient(false));
+});
+
+directionSelect.addEventListener("change", () => generateGradient(false));
+
+randomBtn.addEventListener("click", () => generateGradient(true));
+
+copyBtn.addEventListener("click", copyGradient);
+
