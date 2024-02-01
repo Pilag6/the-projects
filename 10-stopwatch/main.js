dd7e19c555da54cfb401a4bf9d03cf52ef629bd3
akiko-luka <ak.luka051@gmail.com>
2024-02-01 15:27:55 +0100

    add some css

--- 10-stopwatch/main.js
+++ 10-stopwatch/main.js
@@ -2,3 +2,16 @@ const timer = document.getElementById("timer")
 const startBtn = document.getElementById("start")
 const stopBtn = document.getElementById("stop")
 const resetBtn = document.getElementById("reset")
+
+let seconds = 0;
+let timerID = null;
+
+
+
+
+
+
+
+startBtn.addEventListener("click", start)
+stopBtn.addEventListener("click", stop)
+resetBtn.addEventListener("click", reset)
