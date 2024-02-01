3ec84052cbb43d0e88732d242b2016fc557f81f1
akiko-luka <ak.luka051@gmail.com>
2024-02-01 15:58:01 +0100

    add some css; add timer function

--- 10-stopwatch/main.js
+++ 10-stopwatch/main.js
@@ -6,7 +6,21 @@ const resetBtn = document.getElementById("reset")
 let seconds = 0;
 let timerID = null;
 
+// timer update
+function timer(){
+  seconds++
 
+  // time format
+	let sec = seconds % 60;
+	let mins = Math.floor((seconds - (hours * 3600)) / 60);
+  let hours = Math.floor(seconds / 3600);
+
+	if (sec < 10) sec = "0" + sec;
+	if (mins < 10) mins = "0" + mins;
+	if (hours < 10) hours = "0" + hrs;
+
+	timer.innerText = `${hours}:${mins}:${sec}`;
+}
 
 
 
