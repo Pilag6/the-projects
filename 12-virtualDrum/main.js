d80219fe73e0a05816f61c50a948002c94691b7a
Pilag6 <pilag6@gmail.com>
2024-04-04 13:32:03 +0200

    Refactor playSound function to use destructuring and optional chaining

--- 12-virtualDrum/main.js
+++ 12-virtualDrum/main.js
@@ -1,17 +1,13 @@
-function playSound(e) {
-    const audioSound = document.querySelector(`audio[data-key="${e.keyCode}"]`);
-    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+function playSound({ keyCode }) {
+    const audioSound = document.querySelector(`audio[data-key="${keyCode}"]`);
+    const key = document.querySelector(`.key[data-key="${keyCode}"]`);
 
-    if (!audioSound) return;
-
-    audioSound.currentTime = 0;
-    audioSound.play();
-
-    key.classList.add("playing");
-
-    setTimeout(() => {
-        key.classList.remove("playing");
-    }, 100);
+    if (audioSound) {
+        audioSound.currentTime = 0;
+        audioSound.play();
+        key?.classList.toggle("playing");
+        setTimeout(() => key?.classList.toggle("playing"), 50);
+    }
 }
 
 window.addEventListener("keydown", playSound);
