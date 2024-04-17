b4a8d567173e51e0cbe06de1fbcb94d2efdd2b6a
Fatima Badaoui <fatimina615@gmail.com>
2024-04-17 16:32:25 +0200

    remove ripetition from the switch

--- 14-cardFlip/main.js
+++ 14-cardFlip/main.js
@@ -23,10 +23,11 @@ const filterBtns = document.querySelectorAll(".filters button");
 let currentIndex = 0;
 let dataArray = sentences;
 
+// Functions
 function createCard(data) {
-    const card = document.createElement("div");
-    card.classList.add("card");
-    card.innerHTML = `
+  const card = document.createElement("div");
+  card.classList.add("card");
+  card.innerHTML = `
       <div class="front">
         <p>${data.german}</p>
         <h3 class="id">${data.id}</h3>
@@ -37,50 +38,51 @@ function createCard(data) {
         <h3 class="id">${data.id}</h3>
       </div>
     `;
-    cards.innerHTML = card.outerHTML;
-    // cards.innerHTML = ""
-    // cards.appendChild(card)
+  cards.innerHTML = card.outerHTML;
+  // cards.innerHTML = ""
+  // cards.appendChild(card)
 
-    // Attach event listener for the volume icon of the new card
-    const volumeIcon = document.querySelector(".fa-volume-high");
-    volumeIcon.addEventListener("click", () => {
-        const outputText = document.querySelector(".front p").textContent;
-        const speech = new SpeechSynthesisUtterance(outputText);
-        speech.lang = "de-DE";
-        speech.volume = 1;
-        speech.rate = 1;
-        speech.pitch = 1;
-        window.speechSynthesis.speak(speech);
-    });
+  // Attach event listener for the volume icon of the new card
+  const volumeIcon = document.querySelector(".fa-volume-high");
+  volumeIcon.addEventListener("click", () => {
+    const outputText = document.querySelector(".front p").textContent;
+    const speech = new SpeechSynthesisUtterance(outputText);
+    speech.lang = "de-DE";
+    speech.volume = 1;
+    speech.rate = 1;
+    speech.pitch = 1;
+    window.speechSynthesis.speak(speech);
+  });
 }
 
-createCard(sentences[currentIndex]);
-
 function nextCard() {
-    currentIndex++;
-    if (currentIndex > dataArray.length - 1) {
-        currentIndex = 0;
-    }
-    createCard(dataArray[currentIndex]);
+  currentIndex++;
+  if (currentIndex > dataArray.length - 1) {
+    currentIndex = 0;
+  }
+  createCard(dataArray[currentIndex]);
 }
 
 function prevCard() {
-    currentIndex--;
-    if (currentIndex < 0) {
-        currentIndex = dataArray.length - 1;
-    }
-    createCard(dataArray[currentIndex]);
+  currentIndex--;
+  if (currentIndex < 0) {
+    currentIndex = dataArray.length - 1;
+  }
+  createCard(dataArray[currentIndex]);
 }
 
 function flipCard() {
-    const card = document.querySelector(".card");
-    card.classList.toggle("flipped");
+  const card = document.querySelector(".card");
+  card.classList.toggle("flipped");
 }
 
 function randomCard() {
-    currentIndex = Math.floor(Math.random() * dataArray.length);
-    createCard(dataArray[currentIndex]);
+  currentIndex = Math.floor(Math.random() * dataArray.length);
+  createCard(dataArray[currentIndex]);
 }
+
+// INITIAL LOAD
+createCard(sentences[currentIndex]);
 
 // Event Listeners
 nextBtn.addEventListener("click", nextCard);
@@ -89,77 +91,56 @@ flipBtn.addEventListener("click", flipCard);
 randomBtn.addEventListener("click", randomCard);
 
 filterBtns.forEach((button) => {
-    button.addEventListener("click", () => {
-        const category = button.dataset.category;
-        console.log(category);
-        // Remove "active" class from all buttons
-        filterBtns.forEach((btn) => {
-            btn.classList.remove("active");
-        });
-
-        // Add "active" class to the clicked button
-        button.classList.add("active");
-        switch (category) {
-            case "sentences":
-                dataArray = sentences;
-                currentIndex = 0;
-                createCard(dataArray[currentIndex]);
-                break;
-            case "questions":
-                dataArray = questions;
-                currentIndex = 0;
-                createCard(questions[currentIndex]);
-                break;
-            case "adjectives":
-                dataArray = adjectives;
-                currentIndex = 0;
-                createCard(adjectives[currentIndex]);
-                break;
-            case "verbs":
-                dataArray = verbs;
-                currentIndex = 0;
-                createCard(verbs[currentIndex]);
-                break;
-            case "nouns":
-                dataArray = nouns;
-                currentIndex = 0;
-                createCard(nouns[currentIndex]);
-                break;
-            case "colors":
-                dataArray = colors;
-                currentIndex = 0;
-                createCard(colors[currentIndex]);
-                break;
-            case "animals":
-                dataArray = animals;
-                currentIndex = 0;
-                createCard(animals[currentIndex]);
-                break;
-            case "adverbs":
-                dataArray = adverbs;
-                currentIndex = 0;
-                createCard(adverbs[currentIndex]);
-                break;
-            case "family":
-                dataArray = family;
-                currentIndex = 0;
-                createCard(family[currentIndex]);
-                break;
-            case "prepositions":
-                dataArray = prepositions;
-                currentIndex = 0;
-                createCard(prepositions[currentIndex]);
-                break;
-            case "food":
-                dataArray = food;
-                currentIndex = 0;
-                createCard(food[currentIndex]);
-                break;
-            case "conjunctions":
-                dataArray = conjunctions;
-                currentIndex = 0;
-                createCard(conjunctions[currentIndex]);
-                break;
-        }
+  button.addEventListener("click", () => {
+    const category = button.dataset.category;
+    console.log(category);
+    // Remove "active" class from all buttons
+    filterBtns.forEach((btn) => {
+      btn.classList.remove("active");
     });
+
+    // Add "active" class to the clicked button
+    button.classList.add("active");
+    switch (category) {
+      case "sentences":
+        dataArray = sentences;
+        break;
+      case "questions":
+        dataArray = questions;
+        break;
+      case "adjectives":
+        dataArray = adjectives;
+        break;
+      case "verbs":
+        dataArray = verbs;
+        break;
+      case "nouns":
+        dataArray = nouns;
+        break;
+      case "colors":
+        dataArray = colors;
+        break;
+      case "animals":
+        dataArray = animals;
+        break;
+      case "adverbs":
+        dataArray = adverbs;
+        break;
+      case "family":
+        dataArray = family;
+        break;
+      case "prepositions":
+        dataArray = prepositions;
+        break;
+      case "food":
+        dataArray = food;
+        break;
+      case "conjunctions":
+        dataArray = conjunctions;
+        break;
+    }
+    // Reset currentIndex to 0 and create a new card with the new data
+    currentIndex = 0;
+    createCard(dataArray[currentIndex]);
+  });
 });
